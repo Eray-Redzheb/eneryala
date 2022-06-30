c9998c26af364a04d6f51f33995a54d419614e4e
kippie020 <95480751+kippie020@users.noreply.github.com>
2022-06-30 12:25:37 +0200

    kaas

--- energy drank/aboutus.php
+++ energy drank/aboutus.php
@@ -4,32 +4,33 @@
    <meta charset="UTF-8" />
    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <link rel="stylesheet" href="css/style.css">
-   <title>AboutUs</title>
+   <link rel="stylesheet" href="css/aboutus.css">
+   <title>About Us</title>
  </head>
  <body>
-   <nav class="navbar">
-     <!-- LOGO -->
-     <div class="logo">CannaEnergy
-     </div>
-     <!-- NAVIGATION MENU -->
-     <ul class="nav-links">
-       <!-- USING CHECKBOX HACK -->
-       <input type="checkbox" id="checkbox_toggle" />
-       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-       <!-- NAVIGATION MENUS -->
-       <div class="menu">
-         <li><a href="homepage.php">Home</a></li>
-         <li><a href="#">About</a></li>
-         <li class="services">
-           <a href="eventspagina1.php">Events</a>
-           <!-- DROPDOWN MENU -->
-         </li>
-         <li><a href="pricing.php">Pricing</a></li>
-         <li><a href="contact.php">Contact</a></li>
-       </div>
-     </ul>
-   </nav>
+ <nav class="navbar">
+        <!-- LOGO -->
+        <div class="logo">CanaEnergy</div>
+        <!-- NAVIGATION MENU -->
+        <ul class="nav-links">
+          <!-- USING CHECKBOX HACK -->
+          <input type="checkbox" id="checkbox_toggle" />
+          <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+          <!-- NAVIGATION MENUS -->
+          <div class="menu">
+            <li><a href="homepage.php">Home</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li class="services">
+              <a href="eventspagina1.php">Events</a>
+              <!-- DROPDOWN MENU -->
+            </li>
+            <li><a href="pricing.php">Pricing</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="logout.php">Uitloggen</a></li>
+            <img src="images/shop.png" alt="#" height="32px"class="shop">
+          </div>
+        </ul>
+      </nav>
    <div class="text-1">
    <h3>On May 8, 1886, Dr. John Pemberton served the world’s first Coca-Cola at Jacobs' Pharmacy in Atlanta, Ga. 
     From that one iconic drink, we’ve evolved into a total beverage company. 
@@ -39,7 +40,7 @@
 </div>
 <div class="bg-text1">
   <section id="foto-block">
-  <img src="img/card1.jpg">
+  <img src="images/foto.png">
   <h3>n May 8, 1886, Dr. John Pemberton served the world’s first Coca-Cola at Jacobs' Pharmacy in Atlanta, Ga. 
     From that one iconic drink, we’ve evolved into a total beverage company. 
     More than 1.9 billion servings of our drinks are enjoyed in more than 200 countries each day. 
@@ -48,13 +49,13 @@
   </div>
   <div class="bg-text2">
     <section id="foto-block">
-      <img src="img/purpose-776x664.jpeg">  
+      <img src="images/foto1.png">  
       <h3>OUR VISION IS TO BE THE LEADING 24/7 BEVERAGE PARTNER – GROWING WITH OUR CUSTOMERS BY ENSURING WE HAVE A BEVERAGE FOR EVERY OCCASION AROUND THE CLOCK</h3>
     </section>
   </div>
 <div class="bg-text3">
   <section id="foto-block">
-    <img src="img/where-we-operate-29-countries.jpeg">
+    <img src="images/where-we-operate-29-countries.jpeg">
     <h3>We bottle and sell the beverages of The Coca-Cola Company exclusively in our 29 markets. We create value for all our stakeholders by supporting the socio-economic development of the societies in which we operate.</h3>
   </section>
 </div>
